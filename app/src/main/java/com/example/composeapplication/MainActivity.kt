2735437ff695cc87b67d0bd6f170662b046fa560
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2021-08-03 10:22:29 +0300

    Bare TEmplate

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -3,35 +3,18 @@ package com.example.composeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.composeapplication.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
-        }
-    }
-}
-
-data class Message(val author: String, val body: String)
-
-@Composable
-fun MessageCard(msg: Message) {
-    Row {
-        Image(
-            painter = painterResource(id = R.drawable.profile),
-            contentDescription = "Profile Pic"
-        )
-        Column {
-            Text(text = msg.author)
-            Text(text = msg.body)
+            ComposeApplicationTheme {
+                Text("Hello World")
+            }
         }
     }
 }
@@ -39,8 +22,8 @@ fun MessageCard(msg: Message) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+    ComposeApplicationTheme {
+        Text("Hello World")
+    }
 }
 
