d2a7b91f962fdad59770a3af6029c55e4526ea7e
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2021-08-06 07:28:30 +0300

    Setup nav and hilt

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -5,25 +5,62 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeApplicationTheme {
-                Text("Hello World")
+                Navigation()
             }
         }
     }
 }
 
-@Preview
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "pokemon_list_screen") {
+        composable("pokemon_list_screen") {
+            Text("Pokemon")
+        }
+        composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
+            arguments = listOf(
+                navArgument("dominantColor") {
+                    type = NavType.IntType
+                },
+                navArgument("pokemonName") {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val dominantColor = remember {
+                val color = it.arguments?.getInt("dominantColor")
+                color?.let{ Color(it) } ?: Color.White
+            }
+            val pokemonName = remember {
+                it.arguments?.getString("pokemonName")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun PreviewMessageCard() {
     ComposeApplicationTheme {
-        Text("Hello World")
+        Navigation()
     }
 }
 
