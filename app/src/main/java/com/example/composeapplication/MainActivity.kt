f54e5f1a3473cbcdc1899df2ea65aaab9b76f894
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2021-08-06 05:32:16 +0300

    happy meal

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -3,9 +3,24 @@ package com.example.composeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +34,54 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+@Composable
+fun MainScreen() {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .verticalScroll(scrollState)
+            .background(color = Color(0xFFF2F2F2)),
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.happy_meal_small),
+            modifier = Modifier.height(300.dp),
+            contentDescription = "Happy Meal",
+            contentScale = ContentScale.Crop
+        )
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "Happy Meal",
+                style = TextStyle(
+                    fontSize = 26.sp
+                )
+            )
+            Spacer(modifier = Modifier.padding(top = 8.dp))
+            Text(
+                text = "800 calories",
+                style = TextStyle(
+                    fontSize = 17.sp
+                )
+            )
+            Spacer(modifier = Modifier.padding(top = 8.dp))
+            Text(
+                text = "$5.99",
+                style = TextStyle(
+                    color = Color(0xFF85bb65),
+                    fontSize = 17.sp
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun PreviewMessageCard() {
     ComposeApplicationTheme {
-        Text("Hello World")
+        MainScreen()
     }
 }
 
