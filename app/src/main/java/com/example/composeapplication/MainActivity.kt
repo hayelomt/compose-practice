90f3c0f4b5b4a837b9b91d76dde01509423856c4
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2021-08-19 14:59:06 +0300

    ui upto promotion

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -3,18 +3,273 @@ package com.example.composeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeApplicationTheme {
-                Text("Hello World")
+            MainTheme {
+                HomeScreen()
             }
+        }
+    }
+}
+
+@Composable
+fun MainTheme(content: @Composable () -> Unit) {
+    ComposeApplicationTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Box() {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(0.dp, (-30).dp),
+            painter = painterResource(id = R.drawable.bg_main),
+            contentDescription = "Header Background",
+            contentScale = ContentScale.FillWidth
+        )
+        Scaffold(topBar = { AppBar() }, backgroundColor = Color.Transparent) { paddingValues ->
+            Content(paddingValues)
+        }
+    }
+}
+
+@Composable
+fun Content(paddingValues: PaddingValues) {
+    Column(modifier = Modifier.padding(paddingValues)) {
+        Header()
+        Spacer(modifier = Modifier.height(16.dp))
+        Promotions()
+    }
+}
+
+@Composable
+fun Promotions() {
+    val colors = listOf(MaterialTheme.colors.primary, Color(0xFF6EB6F5))
+    LazyRow(
+        Modifier.height(160.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(10) { count ->
+
+            PromotionItem(
+                imagePainter = painterResource(id = R.drawable.promotion),
+                title = "Fruit",
+                subtitle = "Start @",
+                header = "$1",
+                backgroundColor = colors[count % 2]
+            )
+
+        }
+    }
+}
+
+@Composable
+fun PromotionItem(
+    title: String = "",
+    subtitle: String = "",
+    header: String = "",
+    backgroundColor: Color = Color.Transparent,
+    imagePainter: Painter
+) {
+    Card(
+        Modifier.width(300.dp),
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = backgroundColor,
+        elevation = 0.dp
+    ) {
+        Row {
+            Column(
+                Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = title, fontSize = 14.sp, color = Color.White)
+                Text(
+                    text = subtitle,
+                    fontSize = 16.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = header,
+                    fontSize = 28.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Image(
+                painter = imagePainter,
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f),
+                alignment = Alignment.CenterEnd,
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+@Composable
+fun Header() {
+    Card(
+        Modifier
+            .height(64.dp)
+            .padding(horizontal = 16.dp),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(Modifier.fillMaxSize(), verticalAlignment = Alignment.CenterVertically) {
+            QrButton()
+            VerticalDivider()
+            Row(
+                Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .clickable { }
+                    .padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_money),
+                    contentDescription = null,
+                    tint = Color(0xFF6FCF97)
+                )
+                Column(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(text = "$120", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                    Text(text = "Top Up", color = MaterialTheme.colors.primary, fontSize = 12.sp)
+                }
+            }
+            VerticalDivider()
+            Row(
+                Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .clickable { }
+                    .padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_coin),
+                    contentDescription = null,
+                    tint = MaterialTheme.colors.primary
+                )
+                Column(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(text = "$10", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                    Text(text = "Points", color = Color.LightGray, fontSize = 12.sp)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun VerticalDivider() {
+    Divider(
+        color = Color(0xFFF1F1F1),
+        modifier = Modifier
+            .width(1.dp)
+            .height(32.dp)
+    )
+}
+
+@Composable
+fun QrButton() {
+    IconButton(
+        onClick = { /*TODO*/ }, modifier = Modifier
+            .fillMaxHeight()
+            .aspectRatio(1f)
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_scan),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun AppBar() {
+    Row(
+        modifier = Modifier
+            .padding(16.dp)
+            .height(48.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Search For Food, Veggies", fontSize = 12.sp) },
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = "Search"
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Outlined.FavoriteBorder,
+                contentDescription = "",
+                tint = Color.White
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Outlined.Notifications,
+                contentDescription = "",
+                tint = Color.White
+            )
         }
     }
 }
@@ -22,8 +277,8 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun PreviewMessageCard() {
-    ComposeApplicationTheme {
-        Text("Hello World")
+    MainTheme {
+        HomeScreen()
     }
 }
 
--- app/src/main/java/com/example/composeapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composeapplication/ui/theme/Theme.kt
@@ -6,37 +6,18 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Orange,
+    primaryVariant = Orange,
+    secondary = Orange,
 )
 
 @Composable
 fun ComposeApplicationTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
+    val colors =
         LightColorPalette
-    }
 
     MaterialTheme(
         colors = colors,
