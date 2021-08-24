0faf1d818a3cf60f01b9594485d12cb98344b46d
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2021-08-24 08:47:53 +0300

    nasic read write

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -3,19 +3,33 @@ package com.example.composeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
+    private val rdManager: RDManager by lazy { RDManager() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ComposeApplicationTheme {
-                Text("Hello World")
+                AddData {
+                    rdManager.addDummyData()
+                }
             }
         }
+    }
+}
+
+@Composable
+fun AddData(addData: () -> Unit) {
+    Button(onClick = addData) {
+        Text(text = "Add Data")
     }
 }
 
---
+++ app/src/main/java/com/example/composeapplication/RDManager.kt
@@ -0,0 +1,14 @@
+package com.example.composeapplication
+
+import com.google.firebase.database.FirebaseDatabase
+
+class RDManager {
+    private val databaseReference = FirebaseDatabase.getInstance().reference
+    private val database = FirebaseDatabase.getInstance()
+    private val myRef = database.getReference("message")
+
+    fun addDummyData() {
+//        databaseReference('message').setValue("Saving data on android")
+        myRef.setValue("Hello Tomorrow")
+    }
+}
